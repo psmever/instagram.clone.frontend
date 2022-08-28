2bd1ba18d63c2a2afaa66e2057e9adc2eab1fbc7
psmever <psmever@gmail.com>
2022-08-28 23:16:48 +0900

    publish

--- src/modules/RootRoutes.tsx
+++ src/modules/RootRoutes.tsx
@@ -1,21 +1,27 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+
 import DefaultPage from 'src/pages/DefaultPage';
-import LoginPage from 'src/pages/auth/LoginPage';
-import FeedPage from 'src/pages/post/FeedPage';
-import ProfilePage from 'src/pages/user/ProfilePage';
-import ExplorePage from 'src/pages/user/ExplorePage';
-import EditProfilePage from 'src/pages/user/EditProfilePage';
+import LoginPage from 'src/pages/publish/auth/LoginPage';
+import FeedPage from 'src/pages/publish/post/FeedPage';
+import ProfilePage from 'src/pages/publish/user/ProfilePage';
+import ExplorePage from 'src/pages/publish/user/ExplorePage';
+import EditProfilePage from 'src/pages/publish/user/EditProfilePage';
 
 const RootRoutes = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
+        {/* publish pages */}
         <Route path="/default" element={<DefaultPage />} />
-        <Route path="/auth/login" element={<LoginPage />} />
-        <Route path="/post/feed" element={<FeedPage />} />
-        <Route path="/user/profile" element={<ProfilePage />} />
-        <Route path="/user/explore" element={<ExplorePage />} />
-        <Route path="/user/edit-profile" element={<EditProfilePage />} />
+        <Route path="/publish/auth/login" element={<LoginPage />} />
+        <Route path="/publish/post/feed" element={<FeedPage />} />
+        <Route path="/publish/user/profile" element={<ProfilePage />} />
+        <Route path="/publish/user/explore" element={<ExplorePage />} />
+        <Route
+          path="/publish/user/edit-profile"
+          element={<EditProfilePage />}
+        />
+        {/* publish pages */}
       </Routes>
     </BrowserRouter>
   );
--- src/pages/DefaultPage.tsx
+++ src/pages/DefaultPage.tsx
@@ -8,31 +8,31 @@ export default function DefaultPage() {
         <tr>
           <th scope="row">로그인</th>
           <td>
-            <Link to="/auth/login">로그인</Link>
+            <Link to="/publish/auth/login">로그인</Link>
           </td>
         </tr>
         <tr>
           <th scope="row">feed</th>
           <td>
-            <Link to="/post/feed">feed</Link>
+            <Link to="/publish/post/feed">feed</Link>
           </td>
         </tr>
         <tr>
           <th scope="row">프로필</th>
           <td>
-            <Link to="/user/profile">프로필</Link>
+            <Link to="/publish/user/profile">프로필</Link>
           </td>
         </tr>
         <tr>
           <th scope="row">explore</th>
           <td>
-            <Link to="/user/explore">explore</Link>
+            <Link to="/publish/user/explore">explore</Link>
           </td>
         </tr>
         <tr>
           <th scope="row">edit-profile</th>
           <td>
-            <Link to="/user/edit-profile">edit-profile</Link>
+            <Link to="/publish/user/edit-profile">edit-profile</Link>
           </td>
         </tr>
       </tbody>
